19ef372852b3abcfaff1f381606d3c8fea568884
Angus Morrison <contact@angus-morrison.com>
2019-08-07 10:42:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,15 +10,15 @@ def using_pop(array)
   array.pop 
 end
 
-def pop_with_args(array, num)
-  array.pop(num)
+def pop_with_args(array)
+  array.pop(2)
 end
 
 def using_shift(array)
   array.shift 
 end
 
-def shift_with_args(array, num)
+def shift_with_args(array)
   array.shift(2)
 end
 
